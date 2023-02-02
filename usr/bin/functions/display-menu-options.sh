8dd81127f4f8647e8e68110c790f3e4ff9c19722
Jonathan Dyallo <emeraldusk@gmail.com>
2023-02-01 23:12:01 -0300

    --system flag for menu

--- usr/bin/functions/display-menu-options.sh
+++ usr/bin/functions/display-menu-options.sh
@@ -4,26 +4,19 @@
 function random_color() {
     color=$((RANDOM % 6 + 1))
     case $color in
-        1) echo -ne "\e[31m" ;; # red
-        2) echo -ne "\e[32m" ;; # green
-        3) echo -ne "\e[33m" ;; # yellow
-        4) echo -ne "\e[34m" ;; # blue
-        5) echo -ne "\e[35m" ;; # purple
-        6) echo -ne "\e[36m" ;; # cyan
+    1) echo -ne "\e[31m" ;; # red
+    2) echo -ne "\e[32m" ;; # green
+    3) echo -ne "\e[33m" ;; # yellow
+    4) echo -ne "\e[34m" ;; # blue
+    5) echo -ne "\e[35m" ;; # purple
+    6) echo -ne "\e[36m" ;; # cyan
     esac
 }
-
 
 # Display the menu options
 function DisplayMenuOptions() {
 
     source /usr/bin/constants/ascii.sh
-
-    # Check if the language is Spanish
-    if [[ $language == "esp" ]]; then
-        prompt="Ingrese una opción: "
-        menu_options=("${menu_options_spanish[@]}")
-    fi
 
     echo "$menu_top"
     echo "$menu_separator"
@@ -32,7 +25,6 @@ function DisplayMenuOptions() {
         color=$(random_color)
         echo -e "$color ${menu_options[$i]} \e[0m"
     done
-
 
 }
 
--- usr/bin/functions/read-prompt.sh
+++ usr/bin/functions/read-prompt.sh
@@ -1,18 +1,12 @@
 #!bin/bash
 
-# Python functions
-UsdArs="/usr/bin/functions/usd-ars.py"
-
 function ReadPrompt() {
-    # Order is based on usr/bin/constants/menu.sh file, if you are going to edit something from here, edit it there too
-    options=(DiskUsage DeleteTemporary EmptyRecycleBin UninstallPackage SystemInfo MemoryUsage Uptime OpenPorts GetWeather DownloadVideo FindFile $UsdArs exit)
-
     # Read the user's option
     read -p "$prompt" option
 
     # Perform the action corresponding to the chosen option
     if [[ $option -le ${#options[@]} && $option -gt 0 ]]; then
-        ${options[$option-1]}
+        ${options[$option - 1]}
     else
         echo "Invalid option"
     fi
