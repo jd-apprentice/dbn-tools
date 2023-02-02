e806bb4569221f4562aa5b51b02be0eb4161df4a
Jonathan <contacto@jonathan.com.ar>
2023-02-02 02:23:36 +0000

    Feat/dbn 7 sub menues (#21)
    * --system flag for menu
    
    * documentation

--- usr/bin/dbn-tools.sh
+++ usr/bin/dbn-tools.sh
@@ -5,33 +5,16 @@ args=()
 prompt="Enter an option: "
 
 # Source functions
-for file in $(find /usr/bin/ -name '*.sh'); do
-    if [[ $file == *"functions"* ]]; then
-        source "$file"
-    elif [[ $file == *"constants"* ]]; then
-        source "$file"
-    fi
+for file in $(find usr/bin/ -name '*.sh'); do
+  if [[ $file == *"functions"* ]]; then
+    source "$file"
+  elif [[ $file == *"constants"* ]]; then
+    source "$file"
+  fi
 done
 
-
-# Iterate over all the arguments
-for arg in "$@"; do
-  if [[ $arg == "--help" ]]; then
-    source /usr/bin/constants/help.sh # Look for a way to not source this file again
-    for description in "${descriptions[@]}"; do
-      echo "$description"
-    done
-    exit
-  fi
-
-  if [[ $arg == "--esp" ]]; then
-    language="esp"
-    continue
-  fi
-
-  # Otherwise, store the argument in the array
-  args+=("$arg")
-done
+# Read and build the menu
+ReadArguments "$@"
 
 # Display the menu options
 DisplayMenuOptions
